6a40d34469a1b4d351b1165b5045a7a9b4699593
R-J <robin.jurinka@gmx.net>
2013-05-25 02:53:53 +0300

    Update class.discussants.plugin.php
    TODO:
    delete must be considered
    column discussants must be prepopulated for existing discussions
    view
    styling (small space after and before first and last user avatar - first and last poster, could be the same. for long lists a split symbol zigzag line in the middle?

--- class.discussants.plugin.php
+++ class.discussants.plugin.php
@@ -17,26 +17,28 @@ $PluginInfo['Discussants'] = array(
   * DONE 2. write dummy text to my new column
   * DONE 3. debug dummy (timestamp?) to table after each refresh of entry in
   *     discussion table
-  *  4. get users of discussion.
-  *  5. info 4. surely exists as a singe array. find out which
-  *  6. count their comments !!!first one not being 
+  * DONE, DONE KEEPING TRACK 4. get users of discussion.
+  * DONE, NOT NECESSARY 5. info 4. surely exists as a singe array. find out which
+  * DONE, COUNTING WHEN HAPPENING 6. count their comments !!!first one not being 
         a comment but a discussion?!!!
-  *  7. Does this info already exist too?
-  *  8. write serialized array (see structure) to table
+  * DONE, NOT REACHABLE WHEN NEEDED 7. Does this info already exist too?
+  * DONE, BUT CHANGED 8. write serialized array (see structure) to table
   *  9. find out event to hook for discussion index (see indexphotos)
   * 10. search what :hover helper functions exist
   */
+// TODO: comment deletion must be considered!
 
 class DiscussantsPlugin extends Gdn_Plugin {
 
-/* just copied, not understood yet: what is that for?
+ /* just copied, not understood yet: what is that for?
 	public function __construct() {
 		$this->Session = Gdn::Session();
 	}
 */
 
+ /* Just in order to test and debug: eyerytime I call the profile, table gets updated */
   public function ProfileController_Render_Before($Sender) {
-    /* Just in order to test: eyerytime I call the profile, table gets updated */
+   
     $test = serialize(
       array(
           array(2, 8, 100)
@@ -61,12 +63,24 @@ class DiscussantsPlugin extends Gdn_Plugin {
   public function DiscussionModel_AfterSaveDiscussion_Handler($Sender, $Args) {
   
     $DiscussionID = $Args['DiscussionID'];
+    $UserID = $Args['FormPostValues']['InsertUserID'];
+
+    // for first posting no intelligence is needed. Store Count, Percentage, Discussion owner and last commentator
+    $value = array(
+      array($UserID => 1)
+      , array($UserID => 100)
+      , $UserID
+      , $UserID
+    );
     
+    
+    // might be encapsulated in "DiscussantsModel" as DiscussantsInit
     Gdn::SQL()
     ->Update('Discussion')
-    ->Set('Discussants', serialize($Args['Fields']))
+    ->Set('Discussants', serialize($value))
     ->Where('DiscussionID', $DiscussionID)
     ->Put();
+   
 /** LessonsLearned:
   * NEVER, NEVER use githhub as a reference for your development!
   * I spent 4 hours trying to get a hook working which was in the github source
@@ -77,19 +91,15 @@ class DiscussantsPlugin extends Gdn_Plugin {
   * best reference
 */
 
-//UpdateDiscussant($DiscussionID, serialized string)
-
   }
 
   public function CommentModel_AfterSaveComment_Handler($Sender, $Args) {
     $DiscussionID = $Args['FormPostValues']['DiscussionID'];
-    
-    Gdn::SQL()
-    ->Update('Discussion')
-    ->Set('Discussants', serialize($Args))
-    ->Where('DiscussionID', $DiscussionID)
-    ->Put();  
-/** LessonsLearned:
+    $UserID = $Args['FormPostValues']['InsertUserID'];
+
+  $this->DiscussantsUpdate($DiscussionID, $UserID);
+
+    /** LessonsLearned:
   * NOTHING! HA HA! Well, I did the same thing as with DiscussionModel but wait...
   * DiscussionID wasn't passed in $Args explicitly so I dumped $Args to my table 
   * and unserialized it with some online tool in order to find out where I can
@@ -98,15 +108,47 @@ class DiscussantsPlugin extends Gdn_Plugin {
   * or $Args['FormPostValues']['DiscussionID'] for retrieving discussionID ?
   */
   
-  //UpdateDiscussant($DiscussionID, serialized string)
   }
-  
-  
-  public function UpdateDiscussants($DiscussionID, $DiscussantContent) {
+
+ 
+   
+  public function DiscussantsUpdate($DiscussionID, $UserID) {
   // two ways of calculating: either count() on comments or just calculate 
   // with what is already in discussants column
+  // mode = delete will be called when comment is deleted
+    
+    $result = Gdn::SQL()
+      ->Select('Discussants')
+      ->From('Discussion')
+      ->Where('DiscussionID', $DiscussionID)
+      ->Get()->ResultArray();
+    
+    $Discussants = unserialize($result[0]['Discussants']);
+    
+    $Discussants[0][$UserID] += 1;
+    $max = max($Discussants[0]);
+    foreach ($Discussants[1] as $key => $value) {
+      $Discussants[1][$key] = intval(ceil($Discussants[0][$key] / $max * 100));
+    }
+    $Discussants[1][$UserID] = intval(ceil($Discussants[0][$UserID] / $max * 100));
+    
+    $Discussants[3] = $UserID;
+    
+   Gdn::SQL()
+    ->Update('Discussion')
+    ->Set('Discussants', serialize($Discussants))
+    ->Where('DiscussionID', $DiscussionID)
+    ->Put();  
+ /** LessonsLearned:
+   * don't know php yet :-(
+   * it took me awfully long, but is 2am and I'm proud of having reached this point :-)
+   * the only new Garden related thing was the sql but that was easy.
+   * ->ResultArray() instead of ->Result()
+   */
+ 
   }
-  
+
+ 
 /**
   * Add a new column to table Discussion and update list of discussion members
   * each time, a comment in this discussion gets added or deleted
@@ -127,15 +169,8 @@ class DiscussantsPlugin extends Gdn_Plugin {
       * for creating AND extending tables.
       * TODO: find out how to delete a row
       */
-      
-  /*
-  // need this for updating?
-      $RequestArgs = Gdn::Controller()->RequestArgs;
-      if (ArrayHasValue($RequestArgs, 'vanilla')) {
-        ThanksLogModel::RecalculateUserReceivedThankCount();
-      }
-  */    
   }
+   
   public function Setup() {
     $this->Structure();
     /* fill the column! otherwise only discussions with comments later than
